3089a397c40db2ad5719361954676265e29d720c
jgkim <jgkim999@gmail.com>
2023-04-30 14:58:55 +0900

    Serilog

--- Grpc/GrpcService/Program.cs
+++ Grpc/GrpcService/Program.cs
@@ -1,6 +1,12 @@
 using GrpcService.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+// Serilog
+// https://github.com/datalust/dotnet6-serilog-example?ref=blog.datalust.co
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console()
+    .ReadFrom.Configuration(ctx.Configuration));
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
@@ -9,6 +15,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 
 var app = builder.Build();
+// Serilog
+// https://github.com/datalust/dotnet6-serilog-example?ref=blog.datalust.co
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
