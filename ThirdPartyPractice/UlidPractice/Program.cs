2efc6f7cee520a4ffc4d39e79da8219a55b04f03
jgkim <jgkim999@gmail.com>
2023-05-28 14:32:08 +0900

    Ulid

---
+++ ThirdPartyPractice/UlidPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Running;
+using UlidPractice;
+
+internal class Program
+{
+    /// <summary>
+    /// dotnet run -project UlidPractice.csproj -c Release
+    /// </summary>
+    /// <param name="args"></param>
+    private static void Main(string[] args)
+    {
+        var ulid = new UlidBenchmark();
+        ulid.Test1();
+        ulid.Test2();
+        var summary = BenchmarkRunner.Run<UlidBenchmark>();
+    }
+}
