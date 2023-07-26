1b710ce6d205c90923d92d0c38d80e2b224c0766
jgkim <jgkim999@gmail.com>
2023-07-26 06:48:27 +0000

    BehaviorTree

---
+++ Unity/UnityBehaviorTree/Assets/TheKiwiCoder/BehaviourTree/Scripts/Runtime/DecoratorNode.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace TheKiwiCoder
+{
+    public abstract class DecoratorNode : Node
+    {
+
+        [SerializeReference]
+        [HideInInspector]
+        public Node child;
+    }
+}
