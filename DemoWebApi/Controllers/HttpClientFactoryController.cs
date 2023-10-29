822f93a6851dd3d88604f85894f13344e0c2e7ed
jgkim <jgkim999@gmail.com>
2023-10-29 18:16:01 +0900

    HttpClient

--- DemoWebApi/Controllers/HttpClientFactoryController.cs
+++ DemoWebApi/Controllers/HttpClientFactoryController.cs
@@ -1,12 +1,7 @@
 using DemoApplication.Handlers;
-using DemoDomain.Entities;
-using Flurl.Http;
 using Flurl.Http.Configuration;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using RestSharp;
-using IHttpClientFactory = System.Net.Http.IHttpClientFactory;
 
 namespace DemoWebApi.Controllers;
 
@@ -14,25 +9,11 @@ namespace DemoWebApi.Controllers;
 [ApiController]
 public class HttpClientFactoryController : ControllerBase
 {
-    private readonly ILogger<HttpClientFactoryController> _logger;
-    private readonly IHttpClientFactory _clientFactory;
-    private readonly HttpClient _publicApiClient;
-    private readonly HttpClient _jsonPlaceholderClient;
-    private readonly IFlurlClientFactory _flurlClientFactory;
     private readonly IMediator _iMediator;
     
-    public HttpClientFactoryController(
-        ILogger<HttpClientFactoryController> logger,
-        IMediator iMediator,
-        IHttpClientFactory clientFactory,
-        IFlurlClientFactory flurlClientFactory)
+    public HttpClientFactoryController(IMediator iMediator)
     {
-        _logger = logger;
         _iMediator = iMediator;
-        _clientFactory = clientFactory;
-        _publicApiClient = clientFactory.CreateClient("PublicApis");
-        _jsonPlaceholderClient = _clientFactory.CreateClient("JsonPlaceholder");
-        _flurlClientFactory = flurlClientFactory;
     }
     
     [HttpGet]
@@ -45,26 +26,21 @@ public class HttpClientFactoryController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> JsonPlaceHolder()
     {
-        var jpResponse = await _jsonPlaceholderClient.GetAsync("posts");
-        var response = JsonConvert.DeserializeObject<Post[]>(await jpResponse.Content.ReadAsStringAsync());
+        var response = await _iMediator.Send(new JsonPlaceholderClientRequest());
         return Ok(response);
     }
 
     [HttpGet]
     public async Task<IActionResult> RestSharp()
     {
-        // useClientFactory: Set to true if you wish to reuse the HttpClient instance
-        var options = new RestClientOptions("https://jsonplaceholder.typicode.com/");
-        using var restClient = new RestClient(options, useClientFactory: true);
-        var response = await restClient.GetJsonAsync<Post[]>("posts");
+        var response = await _iMediator.Send(new RestClientRequest());
         return Ok(response);
     }
 
     [HttpGet]
     public async Task<IActionResult> Flurl()
     {
-        var client = _flurlClientFactory.Get("https://jsonplaceholder.typicode.com/");
-        var response = await client.Request("posts").GetJsonAsync<Post[]>();
+        var response = await _iMediator.Send(new FlurlClientFactoryRequest());
         return Ok(response);
     }
 }
