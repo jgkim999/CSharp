358db85b4fdbf166478e64688185a539ecc1ce69
KimJoonGwon <jgkim999@gmail.com>
2023-11-15 23:21:17 +0900

    Add Demo project

--- NET8/NET8.AppHost/Program.cs
+++ NET8/NET8.AppHost/Program.cs
@@ -1,3 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var apiservice = builder.AddProject<Projects.NET8_ApiService>("apiservice");
+
+builder.AddProject<Projects.NET8_Web>("webfrontend")
+    .WithReference(apiservice);
+
 builder.Build().Run();
