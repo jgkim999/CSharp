86286c4310086165f57985579337a5c9b58cd624
jgkim <jgkim999@gmail.com>
2024-12-08 13:09:43 +0900

    Add metrics

--- NET8/StRunner/Program.cs
+++ NET8/StRunner/Program.cs
@@ -1,3 +1,6 @@
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using Serilog;
 using StRunner.Exceptions;
 using StRunner.Services;
 
@@ -5,37 +8,85 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile(
+                $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+                optional: true)
+            .AddEnvironmentVariables()
+            .Build();
 
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.AddExceptionHandler<CustomExceptionHandler>();
-
-        builder.Services.AddHealthChecks();
-
-        builder.Services.AddSingleton<K6Service>();
-
-        // Add services to the container.
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+        try
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddSerilog();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+            builder.Services.AddHealthChecks();
+
+            builder.Services.AddOpenTelemetry()
+                .ConfigureResource(r => r.AddService("StRunner"))
+                .WithMetrics(metrics => metrics
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddRuntimeInstrumentation()
+                    .AddProcessInstrumentation()
+                    .AddMeter([
+                        "Microsoft.AspNetCore.Hosting",
+                        "Microsoft.AspNetCore.Server.Kestrel",
+                        "Microsoft.AspNetCore.Diagnostics",
+                        "System.Net.NameResolution",
+                        "System.Runtime",
+                        "Microsoft.Extensions.Diagnostics.HealthChecks",
+                        "Microsoft.Extensions.Diagnostics.ResourceMonitoring",
+                        "Microsoft.Extensions.Hosting",
+                        "System.Http"
+                    ])
+                    .AddPrometheusExporter()
+                );
+
+            builder.Services.AddSingleton<K6Service>();
+
+            // Add services to the container.
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.MapHealthChecks("/health");
+
+            app.MapPrometheusScrapingEndpoint();
+
+            app.Run();
         }
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.MapHealthChecks("/health");
-
-        app.Run();  
+        catch (Exception e)
+        {
+            Log.Error(e, "Unhandled Exception");
+            throw;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
