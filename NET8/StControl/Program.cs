16dc8c62be6eb50d07b6fb9aa69bd375dc803b82
jgkim <jgkim999@gmail.com>
2024-12-08 14:07:53 +0900

    Add log, health,, metrics

--- NET8/StControl/Program.cs
+++ NET8/StControl/Program.cs
@@ -1,23 +1,87 @@
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+
+using Serilog;
+
 using StControl.Components;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    public static void Main(string[] args)
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile(
+                $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+                optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+        try
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddSerilog();
+
+            builder.Services.AddHealthChecks();
+
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddOpenTelemetry()
+                .ConfigureResource(r => r.AddService("StRunner"))
+                .WithMetrics(metrics => metrics
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddRuntimeInstrumentation()
+                    .AddProcessInstrumentation()
+                    .AddMeter([
+                        "Microsoft.AspNetCore.Hosting",
+                        "Microsoft.AspNetCore.Server.Kestrel",
+                        "Microsoft.AspNetCore.Diagnostics",
+                        "System.Net.NameResolution",
+                        "System.Runtime",
+                        "Microsoft.Extensions.Diagnostics.HealthChecks",
+                        "Microsoft.Extensions.Diagnostics.ResourceMonitoring",
+                        "Microsoft.Extensions.Hosting",
+                        "System.Http"
+                    ])
+                    .AddPrometheusExporter()
+                );
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.MapHealthChecks("/health");
+
+            app.MapPrometheusScrapingEndpoint();
+
+            app.Run();
+        }
+        catch (Exception e)
+        {
+            Log.Error(e, "Unhandled Exception");
+            throw;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
 }
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
