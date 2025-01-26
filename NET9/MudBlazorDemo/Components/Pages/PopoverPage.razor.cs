c790364dce8b1b7dc4c43f99d1db0627053af6e3
jgkim <jgkim999@gmail.com>
2025-01-26 23:38:13 +0900

    feat: Add ButtonCopyClipboard

---
+++ NET9/MudBlazorDemo/Components/Pages/PopoverPage.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MudBlazorDemo.Components.Pages;
+
+public partial class PopoverPage : ComponentBase
+{
+    public string TextValue { get; set; } = "Hello, World!";
+    
+    protected async Task OnInitializeAsync()
+    {
+        await base.OnInitializedAsync();
+    }
+}
