b2664ce64d9b806018a07d5815b39858f122c3c2
jgkim <jgkim999@gmail.com>
2025-02-06 22:00:13 +0900

    feat: Demo.Infra 및 Demo.xUnitTest 프로젝트 추가 및 OrderRepository 구현

---
+++ NET9/Demo.Infra/Repositories/OrderRepository.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+
+using MySqlConnector;
+
+using System.Data;
+
+namespace Demo.Infra.Repositories;
+
+public class OrderRepository
+{
+    private string _connectionString = string.Empty;
+
+    public OrderRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public async Task<int> GetOrderCount(string state)
+    {
+        MySqlConnection conn = new(_connectionString);
+        await conn.OpenAsync();
+
+        DynamicParameters parameters = new DynamicParameters();
+        parameters.Add("orderStatus", state);
+        parameters.Add("@total", dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+        //Execute stored procedure and map the returned result to a Customer object  
+        var total = await conn.ExecuteAsync(
+            "GetOrderCountByStatus",
+            parameters,
+            commandType: CommandType.StoredProcedure);
+
+        // Get the output parameter value
+        var outputParamValue = parameters.Get<int>("@total");
+
+        return outputParamValue;
+    }
+
+    public async Task<int> GetOrderCount2(string state)
+    {
+        await using MySqlConnection conn = new(_connectionString);
+        await conn.OpenAsync();
+
+        await using var cmd = conn.CreateCommand();
+        cmd.CommandText = "GetOrderCountByStatus";
+        cmd.CommandType = CommandType.StoredProcedure;
+
+        cmd.Parameters.Add(new MySqlParameter("@state", MySqlDbType.VarChar) { Value = state });
+        cmd.Parameters.Add(new MySqlParameter("@total", MySqlDbType.Int32) { Direction = ParameterDirection.Output });
+
+        await cmd.ExecuteScalarAsync();
+
+        return (int)cmd.Parameters["@total"].Value;
+    }
+}
---
+++ NET9/Demo.xUnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Demo.Infra.Repositories;
+
+namespace Demo.xUnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task OrderRepository()
+    {
+        string connectionString = "Server=192.168.0.47;User ID=root;Password=1234;Database=classicmodels;";
+
+        OrderRepository orderRepository = new OrderRepository(connectionString);
+        var count = await orderRepository.GetOrderCount("Shipped");
+        Assert.Equal(303, count);
+
+        var count2 = await orderRepository.GetOrderCount2("Shipped");
+        Assert.Equal(303, count2);
+    }
+}
