bcf830c7c429098811c5f9b27aba418b82c65079
jgkim <jgkim999@gmail.com>
2025-03-27 00:03:25 +0900

    add: :sparkles: Serilog 및 Microsoft.Extensions.Configuration 패키지 추가 및 로깅 기능 구현

--- NET9/SpectreDemo/Program.cs
+++ NET9/SpectreDemo/Program.cs
@@ -1,13 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Spectre.Console;
+using Serilog;
+using Serilog.Core;
 
-Console.WriteLine("Hello, World!");
-AnsiConsole.Markup("[underline red]Hello[/] World!");
-AnsiConsole.WriteLine();
-AnsiConsole.Write(new Markup("[bold yellow]Hello[/] [red]World![/]"));
-AnsiConsole.WriteLine();
-var table = new Table();
-table.AddColumn(new TableColumn(new Markup("[yellow]Foo[/]")));
-table.AddColumn(new TableColumn("[blue]Bar[/]"));
-AnsiConsole.Write(table);
+using Spectre.Console;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+using Serilog.Extensions.Logging;
+
+using System.IO;
+
+namespace SpectreDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        Console.WriteLine("Hello, World!");
+        AnsiConsole.Markup("[underline red]Hello[/] World!");
+        AnsiConsole.WriteLine();
+        AnsiConsole.Write(new Markup("[bold yellow]Hello[/] [red]World![/]"));
+        AnsiConsole.WriteLine();
+        var table = new Table();
+        table.AddColumn(new TableColumn(new Markup("[yellow]Foo[/]")));
+        table.AddColumn(new TableColumn("[blue]Bar[/]"));
+        AnsiConsole.Write(table);
+        */
+        Logger inner = ConfigureLogger();
+        var microsoftLogger = new SerilogLoggerFactory(inner)
+            .CreateLogger<SpaceLibrary>();
+        
+        SpaceLibrary library = new SpaceLibrary(microsoftLogger);
+
+        microsoftLogger.LogInformation("Start space library");
+
+        library.Run().Wait();
+
+        microsoftLogger.LogInformation("Done with the space library");
+    }
+
+    private static Logger ConfigureLogger()
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        return new LoggerConfiguration()
+            .ReadFrom
+            .Configuration(configuration)
+            .CreateLogger();
+        
+        /*return new LoggerConfiguration()
+            .WriteTo
+            .Spectre()
+            .MinimumLevel.Verbose()
+            .CreateLogger();*/
+    }
+}
