b30c4a6aa5b61e8d119730b11bef19099a086e1e
jgkim <jgkim999@gmail.com>
2025-06-06 14:26:26 +0900

    feat: :sparkles: 초기 설정 및 OpenTelemetry 구성 추가, 테스트 엔드포인트 구현

---
+++ NET9/JsonToLog/Endpoints/Test/TestEndpointV1.cs
@@ -0,0 +1,27 @@
+using FastEndpoints;
+
+using NSwag.AspNetCore;
+
+namespace JsonToLog.Endpoints.Test;
+
+public class TestEndpointV1 : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Get("/api/test");
+        AllowAnonymous();
+        Version(1);
+        Description(x => x
+            .WithName("TestEndpoint V1")
+            .WithSummary("A simple test endpoint")
+            .Produces<object>(200)
+            .Produces(500));
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        // Simulate some processing
+        await Task.Delay(100, ct);
+        await SendOkAsync(new { Message = "Test successful V1" }, ct);
+    }
+}
---
+++ NET9/JsonToLog/Program.cs
@@ -0,0 +1,135 @@
+
+using FastEndpoints;
+using FastEndpoints.Swagger;
+
+using JsonToLog.Configs;
+using JsonToLog.Services;
+
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+using Scalar.AspNetCore;
+
+using Serilog;
+using Serilog.Sinks.OpenTelemetry;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile(
+        $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+        optional: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+OpenTelemetryConfig? openTelemetryConfig = configuration.GetSection("OpenTelemetry").Get<OpenTelemetryConfig>();
+if (openTelemetryConfig is null)
+{
+    throw new NullReferenceException("OpenTelemetry configuration is missing");
+}
+
+ApplicationConfig? applicationConfig = configuration.GetSection("ApplicationSettings").Get<ApplicationConfig>();
+if (applicationConfig is null)
+{
+    throw new NullReferenceException("Application configuration is missing");
+}
+
+Dictionary<string, object> otelAttributes = new()
+{
+    { "service.name", applicationConfig.ServiceName },
+    { "service.version", applicationConfig.Version },
+    { "service.instance.id", Environment.MachineName },
+    { "environment", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production" }
+};
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .Enrich.FromLogContext()
+    .Enrich.WithProperty("ServiceName", applicationConfig.ServiceName)
+    .Enrich.WithProperty("ServiceVersion", applicationConfig.Version)
+    .Enrich.WithProperty("ServiceInstanceId", Environment.MachineName)
+    .Enrich.WithProperty("Environment", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production")
+    .WriteTo.OpenTelemetry(options =>
+    {
+        options.Endpoint = openTelemetryConfig.Endpoint;
+        options.Protocol = openTelemetryConfig.GetProtocol;
+    })
+    .CreateLogger();
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Environment.ApplicationName = applicationConfig.ServiceName;
+    
+    ActivityService.Initialize(applicationConfig.ServiceName, applicationConfig.Version);
+    
+    builder.Services.AddSerilog();
+    
+    var otel = builder.Services.AddOpenTelemetry();
+    otel.ConfigureResource(resource => resource
+        .AddService(applicationConfig.ServiceName));
+    
+    otel.WithMetrics(metrics =>
+    {
+        // Metrics provider from OpenTelemetry
+        metrics.AddAspNetCoreInstrumentation();
+        metrics.AddProcessInstrumentation();
+        metrics.AddRuntimeInstrumentation();
+        metrics.AddHttpClientInstrumentation();
+        // Metrics provides by ASP.NET Core in .NET 8
+        metrics.AddMeter("Microsoft.AspNetCore.Hosting");
+        metrics.AddMeter("Microsoft.AspNetCore.Server.Kestrel");
+        // Metrics provided by System.Net libraries
+        metrics.AddMeter("System.Net.Http");
+        metrics.AddMeter("System.Net.NameResolution");
+        //metrics.AddConsoleExporter();
+        metrics.AddOtlpExporter(o =>
+        {
+            o.Endpoint = new Uri(openTelemetryConfig.Endpoint);
+            o.Protocol = OtlpExportProtocol.Grpc;
+        });
+        metrics.AddPrometheusExporter();
+        //metrics.AddConsoleExporter();
+    });
+    
+    // Add Tracing for ASP.NET Core and our custom ActivitySource and export to Jaeger
+    otel.WithTracing(tracing =>
+    {
+        var probability = openTelemetryConfig.TraceSampleRate;
+        tracing.SetSampler(new TraceIdRatioBasedSampler(probability));
+        tracing.AddSource(ActivityService.Name);
+        tracing.AddAspNetCoreInstrumentation();
+        tracing.AddHttpClientInstrumentation();
+        tracing.AddOtlpExporter(o =>
+        {
+            o.Endpoint = new Uri(openTelemetryConfig.Endpoint);
+            o.Protocol = OtlpExportProtocol.Grpc;
+        });
+        //tracing.AddConsoleExporter();
+    });
+
+    builder.Services
+        .AddFastEndpoints();
+
+    var app = builder.Build();
+
+    app.UseFastEndpoints(c =>
+    {
+        c.Versioning.Prefix = "v";
+    });
+    
+    app.MapPrometheusScrapingEndpoint();
+    
+    app.Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "Ter");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
